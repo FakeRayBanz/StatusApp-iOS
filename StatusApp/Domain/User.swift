061520c261860d14387162e4f39b9872557269ac
Matthew Parker <matthewparker@hotmail.com.au>
2022-11-16 22:22:29 +1000

    Added User.swift

---
+++ StatusApp/Domain/User.swift
@@ -0,0 +1,20 @@
+//
+//  User.swift
+//  StatusApp
+//
+//  Created by Matthew Parker on 16/11/2022.
+//
+
+import Foundation
+
+struct User {
+    var AccountId: Int;
+    var FirstName: String;
+    var LastName: String;
+    var Email: String;
+    var Password: String;
+    var Username: String;
+    var PhoneNumber: String;
+    var Status: String;
+    var Online: Bool;
+}
