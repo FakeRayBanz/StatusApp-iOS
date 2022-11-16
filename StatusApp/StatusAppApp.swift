b78217a2411ff1edb9909d7cefb4313a3affc8ec
Matthew Parker <matthewparker@hotmail.com.au>
2022-11-16 21:05:59 +1000

    Initial Commit

---
+++ StatusApp/StatusAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  StatusAppApp.swift
+//  StatusApp
+//
+//  Created by Matthew Parker on 16/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct StatusAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
