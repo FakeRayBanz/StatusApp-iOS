e3f10bf467bed62534a8a628b72dede88addedf1
Matthew Parker <matthewparker@hotmail.com.au>
2022-11-16 21:40:51 +1000

    Added Message.swift

---
+++ StatusApp/Domain/Message.swift
@@ -0,0 +1,17 @@
+//
+//  Message.swift
+//  StatusApp
+//
+//  Created by Matthew Parker on 16/11/2022.
+//
+
+import Foundation
+
+struct Message {
+    var MessageId: Int;
+    var ChatId: Int;
+    var Data: String;
+    var Created: Date;
+    var LastUpdated: Date;
+    var AuthorId: Int;
+}
